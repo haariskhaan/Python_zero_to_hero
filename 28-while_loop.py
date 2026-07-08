fffa8ec13a46db18e13646d7378ea4bd40f91467
haariskhaan <hhariskhan333@gmail.com>
2026-07-08 20:45:33 +0500

    while loop code

---
+++ 28-while_loop.py
@@ -0,0 +1,24 @@
+# sytax of while loop
+
+# while [condition]:
+#     print("do something")
+
+# jb tk ye true hai ye kaam krtay rho
+
+
+x= 0
+while x<10:
+    print("still lesser value")
+    x= x+1
+    print(x)
+
+
+print("now outside the loop")
+
+
+print("#############################")
+
+# if [condtion]:
+#     print("condition is true")
+
+
