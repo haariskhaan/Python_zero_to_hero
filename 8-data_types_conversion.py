1f02d66879469d459338fc05bf2ee09810e7de84
haariskhaan <hhariskhan333@gmail.com>
2026-07-11 20:38:25 +0500

    Add files via upload

---
+++ 8-data_types_conversion.py
@@ -0,0 +1,28 @@
+a= 1
+b = 1.2
+c = 1j
+
+
+print("######################## before conversion ####################")
+print("type of a is ",type(a))
+print("type of b is ",type(b))
+print("type of c is ",type(c))
+
+
+print(a)
+print(b)
+print(c)
+
+print("######################### after conversion ####################")
+a= float(a)
+b= int(b)
+c= str(c)
+
+print("type of a is ",type(a))
+print("type of b is ",type(b))
+print("type of c is ",type(c))
+
+
+print(a)
+print(b)
+print(c)
