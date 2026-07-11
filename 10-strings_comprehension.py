8785e266ab59c2599faed02d8cb8f0ce110df658
haariskhaan <hhariskhan333@gmail.com>
2026-07-11 20:42:48 +0500

    Add files via upload

---
+++ 10-strings_comprehension.py
@@ -0,0 +1,32 @@
+# getting values from strings
+
+
+str_a = "name is haris"
+str_b = "name is khan"
+
+kuch_b1 =str_a[8]
+kuch_b2 =str_a[9]
+kuch_b3 =str_a[10]
+kuch_b4 =str_a[11]
+kuch_b5 =str_a[12]
+
+print(kuch_b1)
+print(kuch_b2)
+print(kuch_b3)
+print(kuch_b4)
+print(kuch_b5)
+
+
+print("#########################################")
+
+short = str_a[8:13]
+print(short)
+
+short2 = str_a[0:8]
+print(short2)
+
+short3 = str_a[:8]
+print(short3)
+
+short4 = str_a[8:]
+print(short4)
