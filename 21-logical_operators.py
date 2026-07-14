645d96eebd9f0caf255a4101deafc20355595ec0
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:42:08 +0500

    Add files via upload

---
+++ 21-logical_operators.py
@@ -0,0 +1,28 @@
+a= 22
+b= 33
+c =22
+
+
+
+if a == c:
+    print("a is equal to c")
+
+# for and both shuld be True then it will be true (koi ek be false hua to false h)
+if a==c and b>c:
+    print("yes")
+
+
+# koi b ek true ho gi to true h
+if a==c or b>c:
+    print("Nooooo")
+
+
+if not (a==c or b>c):
+    print("true")
+
+
+if True:
+    print("true")
+
+if not True:
+    print("true")
