3714a238ec7465177bfc2916244fe91406ed76d8
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:40:32 +0500

    Add files via upload

---
+++ 17-if_statement.py
@@ -0,0 +1,47 @@
+a=5
+b=5
+
+
+
+# print(a==b)
+
+# if statement syntax
+
+# if boolean or Condition :
+    # if true then execute this
+
+if a==b:
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+
+
+print("outside ended")
+
+
+if a!=b:
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+    print("yes")
+print("outside ended2")
+print("########################## if and else #########################")
+
+
+if a==b:
+    print("yes a is equal to b")
+else:
+    print("yes a is not equal to b")
+
+
+
+
