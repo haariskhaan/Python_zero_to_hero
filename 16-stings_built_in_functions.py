c9300b3813dd1e6df70acc3816b086171f348da1
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:31:57 +0500

    Add files via upload

---
+++ 16-stings_built_in_functions.py
@@ -0,0 +1,68 @@
+# Method		Description
+# capitalize()	Converts the first character to upper case
+#
+
+string = "pakistan"
+print(string.capitalize())
+
+
+
+#
+# count()		Returns the number of times a specified value occurs in a string
+#
+
+
+print(string.count("t"))
+
+# endswith()	Returns true if the string ends with the specified value
+#
+
+print(string.endswith("t"))
+
+
+# find()		Searches the string for a specified value and returns the position of where it was found
+#
+print(string.find("t"))
+
+
+# isdigit()	Returns True if all characters in the string are digits
+#
+
+print(string.isdigit())
+
+
+# islower()	Returns True if all characters in the string are lower case
+
+print(string.islower())
+
+# isnumeric()	Returns True if all characters in the string are numeric
+#
+
+print(string.isnumeric())
+
+# isupper()	Returns True if all characters in the string are upper case
+#
+print(string.isupper())
+
+
+# lower()	Converts a string into lower case
+
+print(string.lower())
+#
+# replace()	Returns a string where a specified value is replaced with a specified value
+
+print(string.replace("p", "ppppp"))
+
+# startswith()	Returns true if the string starts with the specified value
+
+print(string.startswith("n"))
+
+# strip()	Returns a trimmed version of the string
+#
+print(string.strip("t"))
+#
+
+
+# upper()	Converts a string into upper case
+
+print(string.upper())
