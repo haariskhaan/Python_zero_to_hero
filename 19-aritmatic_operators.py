81dede6fdf185d0da10abffc7541490bedac2866
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:41:21 +0500

    Add files via upload

---
+++ 19-aritmatic_operators.py
@@ -0,0 +1,21 @@
+a = 7
+b = 3
+
+result = a + b
+print(result)
+
+
+result = a - b
+print(result)
+
+result = a * b
+print(result)
+
+result = a / b
+print(result)
+
+result = a // b
+print(result)
+
+result = a % b
+result
