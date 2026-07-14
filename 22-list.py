09fe53090e16ab91d78c9efadeb54ef3322d3fe8
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:43:34 +0500

    Add files via upload

---
+++ 22-list.py
@@ -0,0 +1,37 @@
+mylist = ["apple", "banana", "cherry"]
+
+a= 1
+
+print(type(a))
+
+
+print(type(mylist))
+
+index_0 = mylist[0]
+print(index_0)
+
+
+hairs = mylist[1]
+print(haris)
+
+index_2 = mylist[2]
+print(index_2)
+
+
+length_of_list = len(mylist)
+print("length of list is   ", length_of_list)
+
+# add items in list
+
+
+
+mylist.append("peach")
+
+print(mylist)
+
+
+index_3 = mylist[3]
+
+print(index_3)
+
+
