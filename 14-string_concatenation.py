c82bfcf3eab31d2a08ddd6e01476a489b05c67d5
haariskhaan <hhariskhan333@gmail.com>
2026-07-14 18:30:59 +0500

    Add files via upload

---
+++ 14-string_concatenation.py
@@ -0,0 +1,19 @@
+# string concatenation
+
+a = "Hello, World!"
+b = "Pakistan"
+
+c = "1"
+d ="2"
+print(a+b)
+print(c+d)
+
+# stings concatenate
+# integer math operations
+
+
+a = "Hello"
+b = "World"
+c= "        "
+c = a + c + b
+print(c)
