11a394101f3512f7e68aa8dc5ec5ced613aa0052
haariskhaan <hhariskhan333@gmail.com>
2026-07-15 19:10:22 +0500

    Add files via upload

---
+++ 24-remove_items_from_list.py
@@ -0,0 +1,34 @@
+list = [1,2,3,4,5,6,7,8,9]
+
+list.remove(1)
+print(list)
+
+list.remove(8)
+print(list)
+
+
+print("##################### pop items ###########################")
+thislist = ["apple", "banana", "cherry"]
+new_tokri = thislist.pop(1)
+print(thislist)
+
+print(new_tokri)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+
+
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+a = []
+print(a)
