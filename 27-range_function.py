cc7959f3f4412ef53fc00d261226e39d07b51bc6
haariskhaan <hhariskhan333@gmail.com>
2026-07-15 19:11:50 +0500

    Add files via upload

---
+++ 27-range_function.py
@@ -0,0 +1,12 @@
+meri_list = ["apple", "banana", "cherry"]
+
+for i in range (10):
+    print(i)
+
+
+
+print("###################################")
+
+
+for i in range (len(meri_list)):
+    print(i)
