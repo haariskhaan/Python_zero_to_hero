f5f6f9d8718e172e6eead64e84cdf36264babd36
haariskhaan <hhariskhan333@gmail.com>
2026-07-15 19:11:19 +0500

    Add files via upload

---
+++ 26-loop_on_list.py
@@ -0,0 +1,13 @@
+meri_list = ["apple", "banana", "cherry"]
+
+# for [variable] in [list_name]:
+updated_list = []
+
+
+for x in meri_list:
+    print(x)
+    x= x + "karachi"
+    updated_list.append(x)
+
+
+print(updated_list)
