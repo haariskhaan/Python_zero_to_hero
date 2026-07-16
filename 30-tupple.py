6c83a022def595e6f44e486756d9a34db9e79b3b
haariskhaan <hhariskhan333@gmail.com>
2026-07-16 18:14:18 +0500

    Add files via upload

---
+++ 30-tupple.py
@@ -0,0 +1,14 @@
+t = ("sunday", "monday", "tuesday", "wednesday", "thursday", "friday")
+
+print(t)
+
+
+typ_of_t = type(t)
+print(typ_of_t)
+
+t2 = (1,2,3,4,5,6)
+print(t2)
+
+
+for i in range(10):
+    print("hhahaha i am not using i")
